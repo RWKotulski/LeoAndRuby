ba5f368947c259cd48f335957dcb57697c90ffa6
Richard HW Baldwin <richard@empireofaustralia.com>
2024-11-26 09:50:14 +1300

    Add LeonardoController and WebhookGenerator templates
    - Create LeonardoController with initialization and request handling methods
    - Implement WebhookGenerator with initialization and generation methods

---
+++ generators/leoandruby/templates/leonardo_controller.rb
@@ -0,0 +1,9 @@
+class LeonardoController
+  def initialize
+    # Controller initialization code here
+  end
+
+  def handle_request
+    # Request handling logic here
+  end
+end
---
+++ generators/leoandruby/webhook_generator.rb
@@ -0,0 +1,13 @@
+module Generators
+  module LeoAndRuby
+    class WebhookGenerator
+      def initialize
+        # Initialization code here
+      end
+
+      def generate
+        # Generation logic here
+      end
+    end
+  end
+end
